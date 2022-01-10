65f1ed67a1750b1fa284d333ce514c6418be1e60
Blackk <73979580+Blackklegend@users.noreply.github.com>
2022-01-09 23:02:22 -0300

    start
    database preparation, express generation

---
+++ database/tabelas.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+const database = require('./db');
+ 
+const M2_Equipes = database.define('m2_equipes', {
+    idequipe: {
+        type: Sequelize.INTEGER,
+        autoIncrement: false,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.CHAR(150),
+        allowNull: false
+    },
+    ativo: Sequelize.INTEGER
+})
+ 
+const M2_Usuarios = database.define('m2_usuarios', {
+    idusuario: {
+        type: Sequelize.INTEGER,
+        autoIncrement: false,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.CHAR(150),
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.CHAR(150),
+        allowNull: false
+    },
+    login: {
+        type: Sequelize.CHAR(30),
+    },
+    idequipe: {
+        type: Sequelize.INTEGER,
+        references: 'equipes',
+        referencesKey: 'idequipe'
+    },
+    ativo: {
+        type: Sequelize.INTEGER
+    }
+})
+
+M2_Equipes.hasOne(M2_Usuarios)
+
+module.exports = {
+    equipes: M2_Equipes,
+    usuarios: M2_Usuarios
+}
